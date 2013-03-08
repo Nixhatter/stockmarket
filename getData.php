c0d1c471564ffe4c173ceec4aaf46c24efe4a5b4
Dillon <dillon@nixx.co>
2013-03-08 16:29:38 -0500

    Fixed SQL Connection

--- getData.php
+++ getData.php
@@ -3,67 +3,67 @@ include("includes/configuration.php");
 doDB();
 
 function createURL($ticker){
-	$currentMonth = date("n");
-	$currentMonth = $currentMonth - 1;
-	$currentDay = date("j");
-	$currentYear = date("Y");
-	//return "http://ichart.finance.yahoo.com/table.csv?s=$ticker&d=$currentMonth&e=$currentDay&f=$currentYear&g=d&a=$currentMonth&b=$currentDay&c=2009&ignore=.csv";
-    return "http://ichart.finance.yahoo.com/table.csv?s=GOOG&d=3&e=8&f=2013&g=d&a=3&b=$8&c=2009&ignore=.csv";
+    $currentMonth = date("n");
+    $currentMonth = $currentMonth - 1;
+    $currentDay = date("j");
+    $currentYear = date("Y");
+    return "http://ichart.finance.yahoo.com/table.csv?s=$ticker&d=$currentMonth&e=$currentDay&f=$currentYear&g=d&a=11&b=19&c=2011&ignore=.csv";
 }
 
-//Gets content from url amd saves to $content
 function getCSVFile($url, $outputFile){
-	$content = file_get_contents($url);
-	//Delete the top banner
-	$content = str_replace("Date,Open,High,Low,Close,Volume,Adj Close", "", $content);
-	$content = trim($content);
-	file_put_contents($outputFile, $content);
-} 
-
-function fileToDatabase($txtFile, $tableName) {
-	$file = fopen($txtFile, "r");
-	//Loops till end of file
-	while(!feof($file)) {
-		// Takes one line and saves it to $line
-		$line = fgets($file);
-		// Deliminate using a comma, save to array called pieces
-		$pieces = explode(",", $line);
-		$date = $pieces[0];
-		$open = $pieces[1];
-		$high = $pieces[2];
-		$low = $pieces[3];
-		$close = $pieces[4];
-		$volume = $pieces[5];
-		$amount_change = $close-$open;
-		$percent_change = ($amount_change/$open)*100;
-	
-		//Checks if table exists
-		//returns true or false
-		$sql = "SELECT * FROM $tableName";
-		$result = mysql_query($sql);
-		
-		// Create table if it doesnt exist
-		if(!result){
-		$sql = "CREATE TABLE $tableName (date DATE, PRIMATE KEY(date), open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume INT, amount_change FLOAT, percent_change FLOAT;)";
-		mysql_query($sql);
-		}
-		
-		$sql = "INSERT INTO $tableName (date, open, high, low, close, volume, amount_change, percent_change) VALUES ('$date', '$open', '$high', '$low', '$close', '$volume', '$amount_change', '$percent_change')";
-		mysql_query(sql);	
-	}
-	//close file
-	fclose($file);
+    $content = file_get_contents($url);
+    $content = str_replace("Date,Open,High,Low,Close,Volume,Adj Close", "", $content);
+    $content = trim($content);
+    file_put_contents($outputFile, $content);
 }
 
-function main() {
-	$mainTickerFile = fopen("tickerMaster.txt","r");
-	while(!feof($mainTickerFile)) {
-		$companyTicker = fgets($mainTickerFile);
-		$fileURL = createURL($companyTicker);
-		$companyTxtFile = "txtFiles/".$companyTicker.".txt";
-		getCSVFile($fileURL, $companyTxtFile);
-		fileToDatabase($companyTxtFile, $companyTicker);
-		
-		}
+function fileToDatabase($txtFile, $tableName){
+    Global $mysqli;    
+    $file = fopen($txtFile,"r");
+    while(!feof($file)){
+        $line = fgets($file);
+        $pieces = explode(",", $line);
+
+        $date = $pieces[0];
+        $open = $pieces[1];
+        $high = $pieces[2];
+        $low = $pieces[3];
+        $close = $pieces[4];
+        $volume = $pieces[5];
+        $amount_change = $close-$open;
+        $percent_change = ($amount_change/$open)*100;
+        
+        $sql = "SELECT * FROM $tableName";
+        $result = mysqli_query($mysqli, $sql);
+        if(!$result){
+            $sql2 = "CREATE TABLE $tableName (date DATE, PRIMARY KEY(date), open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume INT, amount_change FLOAT, percent_change FLOAT)";
+            if(mysqli_query($mysqli, $sql2) === TRUE) {
+                printf("created table");
+            }
+            elseif(mysqli_query($mysqli, $sql2) === FALSE)  {
+                printf("failed to create table. \n");
+            }
+        }
+        $sql3 = "INSERT INTO $tableName (date, open, high, low, close, volume, amount_change, percent_change) VALUES ('$date','$open','$high','$low','$close','$volume','$amount_change','$percent_change')";
+        mysqli_query($mysqli, $sql3);
+     
+    }
+    fclose($file);
+    mysqli_close($mysqli);
 }
+
+function main(){
+    $mainTickerFile = fopen("tickerMaster.txt","r");
+    while(!feof($mainTickerFile)){
+        $companyTicker = fgets($mainTickerFile);
+        $companyTicker = trim($companyTicker);
+        
+        $fileURL = createURL($companyTicker);
+        $companyTxtFile = "txtFiles/".$companyTicker.".txt";
+        getCSVFile($fileURL, $companyTxtFile);
+        fileToDatabase($companyTxtFile, $companyTicker);
+    }
+}
+
+main();
 ?>
--- includes/configuration.php
+++ includes/configuration.php
@@ -4,7 +4,6 @@ function doDB() {
 	global $mysqli;
 	//connect to server and select database
 	$mysqli = mysqli_connect("localhost", "nixx", "7BBC610C4A815FD0DB49F7094AF36F9D3E0C4EE43B9C380660346C4ACE8403AF", "stockmarket");
-
 	//If the connection fails
 	if(mysqli_connect_errno()){
 		printf("Connect failed: %s\n", mysqli_connect_error());

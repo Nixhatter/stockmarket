c225eb89a8645bffe15071531821444e48482d25
Dillon <dillon@nixx.co>
2013-06-15 11:22:39 -0400

    Updated config file for PDO

--- includes/configuration.php
+++ includes/configuration.php
@@ -1,13 +1,27 @@
 <?php
 
-function doDB() {
-	global $mysqli;
+function pdoDB() {
 	//connect to server and select database
-	$mysqli = mysqli_connect("localhost", "nixx", "7BBC610C4A815FD0DB49F7094AF36F9D3E0C4EE43B9C380660346C4ACE8403AF", "stockmarket");
-	//If the connection fails
-	if(mysqli_connect_errno()){
-		printf("Connect failed: %s\n", mysqli_connect_error());
-		exit();
-	}
-}
+    $username = "nixx";
+    $password = "7BBC610C4A815FD0DB49F7094AF36F9D3E0C4EE43B9C380660346C4ACE8403AF";
+    $id = "AAPL";
+    try {
+        $conn = new PDO('mysql:host=localhost;dbname=stockmarket', $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+         
+        $stmt = $conn->prepare('SELECT * FROM :id');
+        $stmt->execute(array('id' => $id));
+     
+        $result = $stmt->fetchAll();
+ 
+      if ( count($result) ) { 
+        foreach($result as $row) {
+          print_r($row);
+        }   
+      } else {
+        echo "No rows returned.";
+      }
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
 ?>
--- index.php
+++ index.php
@@ -7,11 +7,7 @@
 </head>
 <body>
 <h1>Stock Market 0.01</h1>
-<script src="//www.gmodules.com/ig/ifr?url=http://hosting.gmodules.com/ig/gadgets/file/100999652958232494854/livequotes_major_us_indices.xml&amp;synd=open&amp;w=480&amp;h=240&amp;title=&amp;border=%23ffffff%7C3px%2C1px+solid+%23999999&amp;output=js"></script>
 
-<form action="getData.php" method="get">
-	<input type = "submit" value = "Gather Historical Data">
-</form>
 
 </body>
 </html>
